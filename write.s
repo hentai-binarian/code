890d6368593de38908e09d143f50271f7932f7e5
binaryen <132439355+binaryen@users.noreply.github.com>
2023-05-04 11:59:07 +0900

    Update write.s

--- write.s
+++ write.s
@@ -4,9 +4,9 @@ _start:
   push rbp
   mov rbp, rsp
   sub rsp, 0x8
-  mov QWORD PTR [rsp], 0x41
-  mov rax, 1
-  mov rdx, 8
+  mov QWORD PTR [rsp], 0x41   ; 文字列"A"
+  mov rax, 1                  ; write()で画面に出力
+  mov rdx, 8                
   mov rdi, 1
   mov rsi, rsp
   syscall
