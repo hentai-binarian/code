b4a495145a7632718e6ba32dbb5ab3a23ffbea5c
変態バイナリアン <132439355+hentai-binarian@users.noreply.github.com>
2023-09-13 16:54:03 +0900

    Add files via upload

--- write.s
+++ write.s
@@ -5,11 +5,11 @@ _start:
   mov rbp, rsp
   sub rsp, 0x8
   mov DWORD PTR [rbp], 0x41
-  mov rax, 1      # write()
-  mov rdx, 1      # 文字数1
-  mov rdi, 1      # 出力
-  lea rsi, [rbp]  #文字のアドレス
-  syscall         # 画面に出力
+  mov rax, 1                    # write()
+  mov rdx, 1                    # 文字数1
+  mov rdi, 1                    # 出力
+  lea rsi, [rbp]                #文字のアドレス
+  syscall                       # 画面に出力
 
-  mov rax, 60     # exit() 
-  syscall         # プログラムを終了
+  mov rax, 60                   # exit() 
+  syscall                       # プログラムを終了
