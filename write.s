ce6ddeba389f914235525395587c58ce4ca41587
変態バイナリアン <132439355+hentai-binarian@users.noreply.github.com>
2023-09-13 16:52:50 +0900

    Add files via upload

--- write.s
+++ write.s
@@ -1,15 +1,15 @@
-.global _start
 .intel_syntax noprefix
+.global _start
 _start:
   push rbp
   mov rbp, rsp
   sub rsp, 0x8
-  mov QWORD PTR [rsp], 0x41   ; 文字列"A"
-  mov rax, 1                  ; write()で画面に出力
-  mov rdx, 8                
-  mov rdi, 1
-  mov rsi, rsp
-  syscall
+  mov DWORD PTR [rbp], 0x41
+  mov rax, 1      # write()
+  mov rdx, 1      # 文字数1
+  mov rdi, 1      # 出力
+  lea rsi, [rbp]  #文字のアドレス
+  syscall         # 画面に出力
 
-  mov rax, 60
-  syscall
+  mov rax, 60     # exit() 
+  syscall         # プログラムを終了
