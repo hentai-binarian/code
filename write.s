44ea031d382e5f433c04af79f6d78abeec69cc39
変態バイナリアン <132439355+hentai-binarian@users.noreply.github.com>
2023-09-13 16:55:42 +0900

    Update write.s

--- write.s
+++ write.s
@@ -2,13 +2,13 @@
 .global _start
 _start:
   push rbp
-  mov rbp, rsp
-  sub rsp, 0x8
-  mov DWORD PTR [rbp], 0x41
+  mov rbp, rsp                  # スタックの確保
+  sub rsp, 0x8                　# ８バイト確保
+  mov DWORD PTR [rbp], 0x41     # 文字列”A"
   mov rax, 1                    # write()
   mov rdx, 1                    # 文字数1
   mov rdi, 1                    # 出力
-  lea rsi, [rbp]                #文字のアドレス
+  lea rsi, [rbp]                # 文字のアドレス
   syscall                       # 画面に出力
 
   mov rax, 60                   # exit() 
